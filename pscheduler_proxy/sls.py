080cb42d7c03a85211835611f26de650651d8eb1
Erik Reid <erik.reid@geant.org>
2018-05-22 12:51:08 +0200

    store responses separately per host

--- pscheduler_proxy/sls.py
+++ pscheduler_proxy/sls.py
@@ -9,6 +9,7 @@ logging.basicConfig(level=logging.DEBUG)
 SLS_BOOTSTRAP_URL = "http://ps-west.es.net:8096/lookup/activehosts.json"
 CACHED_SERVICE_LIST_FILENAME = "/tmp/sls-cache.json"
 
+
 def load_sls_hosts(bootstrap_url):
     rsp = requests.get(
         bootstrap_url,
@@ -24,39 +25,44 @@ def load_services(bootstrap_url):
 
     session = FuturesSession(max_workers=10)
 
-    jobs = [session.get(
+    jobs = {url: session.get(
                 url,
                 headers={"Accept": "application/json"},
                 params={"type": "service"})
-            for url in load_sls_hosts(bootstrap_url)]
+            for url in load_sls_hosts(bootstrap_url)}
 
-    all_responses = []
-    for j in jobs:
-        rsp = j.result()
-        logging.debug("rsp status_code: %d" % rsp.status_code)
-        assert rsp.status_code == 200
-        all_responses += rsp.json()
-
+    all_responses = {}
+    for url, job in jobs.items():
+        rsp = job.result()
+        if rsp.status_code == 200:
+            all_responses[url] = rsp.json()
+        else:
+            logging.error("'%s' returned status code %d" % (url, rsp.status_code))
+            all_responses[url] = []
     return all_responses
+
 
 def update_cached_mps(bootstrap_url=SLS_BOOTSTRAP_URL, cache_filename=CACHED_SERVICE_LIST_FILENAME):
     with open(cache_filename, "w") as f:
         f.write(json.dumps(load_services(bootstrap_url)))
 
+
 def load_mps(tool, cache_filename=CACHED_SERVICE_LIST_FILENAME):
     with open(cache_filename) as f:
         sls_cache = json.loads(f.read())
-    for s in sls_cache:
-        #logging.debug("s['service-type'] : %s" % s["service-type"])
-        if "pscheduler" not in s["service-type"]:
-            continue
-        if "pscheduler-tools" not in s:
-            logging.debug("'pscheduler-tools' not in s: " + json.dumps(s))
-            continue
-        if tool not in s["pscheduler-tools"]:
-            continue
-        for l in s["service-locator"]:
-          yield l
+    for url in sls_cache.keys():
+        for s in sls_cache[url]:
+            #logging.debug("s['service-type'] : %s" % s["service-type"])
+            if "pscheduler" not in s["service-type"]:
+                continue
+            if "pscheduler-tools" not in s:
+                logging.debug("'pscheduler-tools' not in s: " + json.dumps(s))
+                continue
+            if tool not in s["pscheduler-tools"]:
+                continue
+            for l in s["service-locator"]:
+              yield l
+
 
 if __name__ == "__main__":
     update_cached_mps()
