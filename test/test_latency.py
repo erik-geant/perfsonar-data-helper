11360d4cc6f57a43d2087bed1c423e1620bdc943
Erik Reid <erik.reid@geant.org>
2018-05-24 08:51:39 +0200

    refactored the way test data is defined

--- test/test_latency.py
+++ test/test_latency.py
@@ -17,48 +17,48 @@ DELAY_RESPONSE_SCHEMA = {
 SOURCE = "perfsonar-nas.asnet.am"
 DESTINATION = "perfsonar-probe.ripe.net"
 
-RESPONSE_DATA = {
-    "https://perfsonar-nas.asnet.am/pscheduler/tasks": [
-        "task-init-response.txt"
-    ],
-    "https://perfsonar-nas.asnet.am/pscheduler/tasks/6f86acbf-ee3b-497d-857b-6c7975f1ac00/runs/first": [
-        "task-0.json",
-        "task-1.json",
-        "task-2.json",
-        "task-3.json",
-        "task-4.json",
-        "task-5.json",
-        "task-6.json",
-        "task-7.json",
-        "task-8.json",
-        "task-9.json",
-        "task-10.json",
-        "task-11.json",
-        "task-12.json",
-        "task-13.json",
-        "task-14.json"
-    ]
-}
+RESPONSE_DATA = [
+    {
+        "url": "https://perfsonar-nas.asnet.am/pscheduler/tasks",
+        "method": responses.POST,
+        "data_filenames": ["task-init-response.txt"]
+    },
+    {
+        "url": "https://perfsonar-nas.asnet.am/pscheduler/tasks/6f86acbf-ee3b-497d-857b-6c7975f1ac00/runs/first",
+        "method": responses.GET,
+        "data_filenames": [
+            "task-0.json",
+            "task-1.json",
+            "task-2.json",
+            "task-3.json",
+            "task-4.json",
+            "task-5.json",
+            "task-6.json",
+            "task-7.json",
+            "task-8.json",
+            "task-9.json",
+            "task-10.json",
+            "task-11.json",
+            "task-12.json",
+            "task-13.json",
+            "task-14.json"
+        ]
+    }
+]
 
 
 def mock_latency_responses():
 
     data_path = os.path.join(os.path.dirname(__file__), "latency")
-    for url, filenames in RESPONSE_DATA.items():
+    for rsp in RESPONSE_DATA:
 
-        for fn in filenames:
+        for fn in rsp["data_filenames"]:
             with open(os.path.join(data_path, fn)) as f:
                 body = f.read()
 
             responses.add(
-                responses.GET,
-                url,
-                body=body,
-                match_querystring=False)
-
-            responses.add(
-                responses.POST,
-                url,
+                rsp["method"],
+                rsp["url"],
                 body=body,
                 match_querystring=False)
 
