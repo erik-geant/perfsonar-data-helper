4ac7e9d271e7734130ea6735b6cd0bbb6136a1ac
Erik Reid <erik.reid@geant.org>
2018-06-01 10:12:39 +0200

    share duplicate pscheduler client code

--- perfsonar_data_helper/throughput.py
+++ perfsonar_data_helper/throughput.py
@@ -1,33 +1,10 @@
 import json
 import logging
-import time
 
-import requests
-
-
-def _get_task_result(task_url, delay_seconds):
-    logging.debug("task result url: %s" % (task_url + "/runs/first"))
-
-    while True:
-        rsp = requests.get(
-            task_url + "/runs/first",
-            verify=False,
-            headers={"Accept": "application/json"})
-
-        assert rsp.status_code == 200
-        result = rsp.json()
-        logging.debug("task state: %s" % result["state"])
-        assert result["state"] in {"pending", "on-deck", "running", "finished" }
-        if result["state"] == "finished":
-#            logging.debug("task result: " + json.dumps(result))
-            return result
-        if delay_seconds > 0:
-            time.sleep(delay_seconds)
+from perfsonar_data_helper.pscheduler import client
 
 
 def get_raw(source, destination, delay_seconds):
-
-    mp_hostname = source
 
     test_spec = {
         "source": source, 
@@ -47,22 +24,11 @@ def get_raw(source, destination, delay_seconds):
         }
     }
 
-    mp_url = 'https://%s/pscheduler/tasks' % mp_hostname
-    logging.debug("mp url: '%s'" % mp_url)
-
-    logging.debug("request data: %s" % json.dumps(test_data))
-
-    rsp = requests.post(
-        mp_url,
-        verify=False,
-        json=test_data)
-    
-    assert rsp.status_code == 200
-
-    logging.debug("task created: %s" % rsp.text)
-    task_result = _get_task_result(
-        rsp.text.rstrip().replace('"', ''),
+    task_url = client.create_task(source, test_data)
+    task_result = client.get_task_result(
+        task_url,
         delay_seconds)
+
     if "result" in task_result:
         return task_result["result"]["intervals"]
     elif "result-merged" in task_result:
