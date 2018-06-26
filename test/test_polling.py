febae50fe31cd5f536a84a1e2bcdf5ef43f40e02
Erik Reid <erik.reid@geant.org>
2018-06-26 21:19:29 +0200

    initial polling test (unfinished)

--- test/test_polling.py
+++ test/test_polling.py
@@ -1,7 +1,30 @@
 import json
 import logging
 
+from jsonschema import validate
 import pytest
+import responses
+
+STATUS_RESPONSE_SCHEMA = {
+    "$schema": "http://json-schema.org/draft-06/schema#",
+    "type": "object",
+    "properties": {
+        "type": {
+            "type": "string",
+            "enum": ["status", "complete"]
+        },
+        "message": {
+            "type": "string",
+            "enum": ["scheduled", "pending", "on-deck", "running"]
+        },
+        "time": {
+            "type": "string",
+            "format": "date-time"
+        },
+        "data": {}
+    },
+    "required": ["type", "time"]
+}
 
 logging.basicConfig(level=logging.DEBUG)
 test_data = [
@@ -29,7 +52,6 @@ def test_bad_request_payload(payload, client):
         data=json.dumps(payload),
         headers={'content-type': 'application/json'}
     )
-
     assert rv.status_code == 400
 
 
@@ -39,5 +61,40 @@ def test_bad_request_content_type(client):
         data=json.dumps(dict(type="aaa", source="sfsfda", destination="xyz")),
         headers={'content-type': 'test/html'}
     )
-
     assert rv.status_code == 415
+
+
+@responses.activate
+def test_latency_happy_flow(client, mocked_latency_test_data):
+    rv = client.post(
+        "/pscheduler/measurement",
+        data=json.dumps({
+            "type": "latency",
+            "source": mocked_latency_test_data["source"],
+            "destination": mocked_latency_test_data["destination"]
+        }),
+        headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    )
+    assert rv.status_code == 200
+    response_payload = json.loads(rv.data.decode("utf-8"))
+    validate(response_payload, STATUS_RESPONSE_SCHEMA)
+    assert response_payload["type"] == "status"
+    assert "message" in response_payload \
+           and response_payload["message"] == "scheduled"
+
+    for _ in range(20):  # test data ends in fewer than 20 iterations
+        rv = client.get("/pscheduler/status")
+        assert rv.status_code == 200
+        response_payload = json.loads(rv.data.decode("utf-8"))
+        validate(response_payload, STATUS_RESPONSE_SCHEMA)
+
+        if response_payload["type"] != "complete":
+            assert "message" in response_payload;
+            continue
+
+        break
+
+    assert False, "too many test iterations"
