7f274e913a9bdf3221b192cd0208a9d049745593
Erik Reid <erik.reid@geant.org>
2019-03-19 16:53:13 +0100

    bumped version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="perfsonar_data_helper",
-    version="0.10",
+    version="0.11",
     author="GEANT",
     author_email="swd@geant.org",
     description="wrapper for pscheduler i/o",
